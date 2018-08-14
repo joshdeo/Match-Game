8f57078507beb581c8b8fab86bcce90b98125f84
joshdeo <40075642+joshdeo@users.noreply.github.com>
2018-08-13 23:03:30 -0400

    jQuery start

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -19,14 +19,21 @@ MatchGame.generateCardValues = function() {
   }
   return random;
 }
-/*  Converts card values to jQuery card objects and adds them to the supplied game object.*/
 
-MatchGame.renderCards = function(cardValues, $game) {
+/*Converts card values to jQuery card objects and adds them to the supplied game object.*/
+$(document).ready(function() {
+      MatchGame.renderCards = function(cardValues, $game) {
+        var colors = {
+          
+        }
+        $game.empty();
 
-};
 
-/*Flips over a given card and checks to see if two cards are flipped over. Updates styles on flipped cards depending whether they are a match or not.*/
+      }
 
-MatchGame.flipCard = function($card, $game) {
+      /*Flips over a given card and checks to see if two cards are flipped over. Updates styles on flipped cards depending whether they are a match or not.*/
 
-};
+      MatchGame.flipCard = function($card, $game) {
+
+      };
+    }
