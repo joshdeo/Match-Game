f0fe996f1719411aced82d33258c8fb70cea8b2b
joshdeo <40075642+joshdeo@users.noreply.github.com>
2018-08-13 20:47:12 -0400

    0-32 complete match game

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,46 +1,31 @@
 var MatchGame = {};
 
-/*
-  Sets up a new game after HTML document has loaded.
-  Renders a 4x4 board of cards.
-*/
+/*Sets up a new game after HTML document has loaded. Renders a 4x4 board of cards.*/
 
-/*
-  Generates and returns an array of matching card values.
- */
+/*Generates and returns an array of matching card values.*/
+
 MatchGame.generateCardValues = function() {
   var values = []
-  values.splice();
-};
-
-for (var loop = 1; loop <= 8; loop++) {
-  console.log('Working' + loop);
-  values.push(loop);
-  values.push(loop);
+  for (var loop = 1; loop <= 8; loop++) {
+    console.log('Working' + loop);
+    values.push(loop);
+    values.push(loop);
+  }
+  var random = [];
+  while (16 > random.length) {
+    let num = Math.floor(Math.random() * values.length);
+    var randNum = values.splice(num, 1);
+    random.push(randNum);
+  }
+  return random;
 }
-
-var random = [];
-
-while (16 > random.length) {
-  let num = math.floor(math.random() * values.length);
-  var randNum = values.splice(num, 1);
-  random.push(randNum);
-}
-
-return random
-/*
-  Converts card values to jQuery card objects and adds them to the supplied game
-  object.
-*/
+/*  Converts card values to jQuery card objects and adds them to the supplied game object.*/
 
 MatchGame.renderCards = function(cardValues, $game) {
 
 };
 
-/*
-  Flips over a given card and checks to see if two cards are flipped over.
-  Updates styles on flipped cards depending whether they are a match or not.
- */
+/*Flips over a given card and checks to see if two cards are flipped over. Updates styles on flipped cards depending whether they are a match or not.*/
 
 MatchGame.flipCard = function($card, $game) {
 
