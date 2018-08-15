eb7ce8a449db2b8e8e8c158d58900212abf98a04
joshdeo <40075642+joshdeo@users.noreply.github.com>
2018-08-14 21:36:54 -0400

    jQuery(5)

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -3,30 +3,29 @@ var MatchGame = {};
 /*Sets up a new game after HTML document has loaded. Renders a 4x4 board of cards.*/
 
 /*Generates and returns an array of matching card values.*/
-
-MatchGame.generateCardValues = function() {
-  var values = []
-  for (var loop = 1; loop <= 8; loop++) {
-    console.log('Working' + loop);
-    values.push(loop);
-    values.push(loop);
-  }
-
-  var random = [];
-
-  while (16 > random.length) {
-    let num = Math.floor(Math.random() * values.length);
-    var randNum = values.splice(num, 1);
-    random.push(randNum);
-  }
-  return random;
-}
-
-/*Converts card values to jQuery card objects and adds them to the supplied game object.*/
 $(document).ready(function() {
+      MatchGame.generateCardValues = function() {
+        var values = []
+        for (var loop = 1; loop <= 8; loop++) {
+          console.log('Working' + loop);
+          values.push(loop);
+          values.push(loop);
+        }
+
+        var random = [];
+
+        while (16 > random.length) {
+          let num = Math.floor(Math.random() * values.length);
+          var randNum = values.splice(num, 1);
+          random.push(randNum);
+        }
+        return random;
+      }
+
+      /*Converts card values to jQuery card objects and adds them to the supplied game object.*/
       MatchGame.renderCards = function(cardValues, $game) {
-        for (var object = 0; object < cardValues.length; object++) {
-          var colors = 'hsl(25, 85%, 65%)'
+        var colors = [
+          'hsl(25, 85%, 65%)'
           'hsl(55, 85%, 65%)'
           'hsl(90, 85%, 65%)'
           'hsl(160, 85%, 65%)'
@@ -34,10 +33,17 @@ $(document).ready(function() {
           'hsl(265, 85%, 65%)'
           'hsl(310, 85%, 65%)'
           'hsl(360, 85%, 65%)'
-        }
+        ];
 
         $game.empty();
         $game.data('isFlipped')
+
+        for (var object = 0; object < cardValues.length; object++) {
+          var data =
+            value:
+            color:
+            isFlipped:
+        };
       }
 
       /*Flips over a given card and checks to see if two cards are flipped over. Updates styles on flipped cards depending whether they are a match or not.*/
