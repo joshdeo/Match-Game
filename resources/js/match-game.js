2bec28b4311bd52cd3381e19e4aaf8330c96f647
joshdeo <40075642+joshdeo@users.noreply.github.com>
2018-08-14 21:29:08 -0400

    jQuery(4)

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -11,7 +11,9 @@ MatchGame.generateCardValues = function() {
     values.push(loop);
     values.push(loop);
   }
+
   var random = [];
+
   while (16 > random.length) {
     let num = Math.floor(Math.random() * values.length);
     var randNum = values.splice(num, 1);
@@ -23,13 +25,24 @@ MatchGame.generateCardValues = function() {
 /*Converts card values to jQuery card objects and adds them to the supplied game object.*/
 $(document).ready(function() {
       MatchGame.renderCards = function(cardValues, $game) {
+        for (var object = 0; object < cardValues.length; object++) {
+          var colors = 'hsl(25, 85%, 65%)'
+          'hsl(55, 85%, 65%)'
+          'hsl(90, 85%, 65%)'
+          'hsl(160, 85%, 65%)'
+          'hsl(220, 85%, 65%)'
+          'hsl(265, 85%, 65%)'
+          'hsl(310, 85%, 65%)'
+          'hsl(360, 85%, 65%)'
+        }
+
         $game.empty();
-        var $object = $('.card') 
+        $game.data('isFlipped')
       }
 
-/*Flips over a given card and checks to see if two cards are flipped over. Updates styles on flipped cards depending whether they are a match or not.*/
+      /*Flips over a given card and checks to see if two cards are flipped over. Updates styles on flipped cards depending whether they are a match or not.*/
 
-MatchGame.flipCard = function($card, $game) {
+      MatchGame.flipCard = function($card, $game) {
 
-  };
-}
+      };
+    }
