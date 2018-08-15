6f56baa7fc79595a277bc9df27975df7d7da111a
joshdeo <40075642+joshdeo@users.noreply.github.com>
2018-08-15 16:30:31 -0400

    jQuery(7)

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -25,25 +25,26 @@ $(document).ready(function() {
       /*Converts card values to jQuery card objects and adds them to the supplied game object.*/
       MatchGame.renderCards = function(cardValues, $game) {
         var colors = [
-          'hsl(25, 85%, 65%)'
-          'hsl(55, 85%, 65%)'
-          'hsl(90, 85%, 65%)'
-          'hsl(160, 85%, 65%)'
-          'hsl(220, 85%, 65%)'
-          'hsl(265, 85%, 65%)'
-          'hsl(310, 85%, 65%)'
-          'hsl(360, 85%, 65%)'
+          'hsl(25, 85%, 65%)',
+          'hsl(55, 85%, 65%)',
+          'hsl(90, 85%, 65%)',
+          'hsl(160, 85%, 65%)',
+          'hsl(220, 85%, 65%)',
+          'hsl(265, 85%, 65%)',
+          'hsl(310, 85%, 65%)',
+          'hsl(360, 85%, 65%)',
         ];
 
         $game.empty();
-        $game.data('flippedCards',[]);
+        $game.data('flippedCards', []);
+
 
         for (var object = 0; object < cardValues.length; object++) {
           var value = cardValues[object];
           var color = color[value - 1];
           var data = {
-            value:
-            color:
+            value: value,
+            color: color,
             isFlipped: false
           }
         }
@@ -51,7 +52,7 @@ $(document).ready(function() {
         var $cardElement = $('<div class="col-md-9">')
         $cardElement.data();
 
-
+        $game.append($cardElement);
       };
 
       /*Flips over a given card and checks to see if two cards are flipped over. Updates styles on flipped cards depending whether they are a match or not.*/
@@ -59,4 +60,3 @@ $(document).ready(function() {
       MatchGame.flipCard = function($card, $game) {
 
       };
-    }
